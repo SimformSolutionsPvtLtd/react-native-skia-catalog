4c125b26fba83ed0623110d3de1a08e684befd3e
Chetan Godiya <chetan.g@simformsolutions.com>
2022-09-13 15:38:22 +0530

    changes

--- src/components/FlickerLoading/FlickerLoading.tsx
+++ src/components/FlickerLoading/FlickerLoading.tsx
@@ -1,23 +1,46 @@
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-import React from "react";
+import { Canvas, Circle, Fill, Group } from "@shopify/react-native-skia";
+import React, { useEffect, useRef } from "react";
 import { Colors } from "../../theme";
 import useFlikerLoading from "./hooks/useFlikerLoading";
 
 const FlickerLoading = () => {
   const size = 256;
+  const ref = useRef(null)
   const r = size * 0.33;
-  const strokeWidth = 5;
+  const strokeWidth = 10;
   const { animatedX, animatedXForMaskedView, animatedZIndex } =
     useFlikerLoading(r);
 
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Group color={Colors.black}>
-        <Circle cx={animatedX} cy={r} r={r} />
-        <Group style="stroke" strokeWidth={strokeWidth}>
-          <Circle cx={animatedXForMaskedView} cy={r} r={r} />
-        </Group>
+    <Canvas style={{ flex: 1 }} ref={ref}>
+      {/* <Circle cx={r} cy={r} r={r} color="#51AFED" /> */}
+
+      {/* The paint is inherited by the following sibling and descendants. */}
+
+      {/* <Group color="lightblue" style="stroke" strokeWidth={10}>
+  <Circle cx={r} cy={r} r={r } />
+</Group> */}
+      <Circle cx={animatedXForMaskedView} cy={r} r={r - 10} color="white" />
+      <Group color="lightblue" style="stroke" strokeWidth={10}>
+        <Circle cx={animatedXForMaskedView} cy={r} style="stroke" r={r - 10} />
+        <Circle
+          color={'black'}
+          style="stroke"
+          cx={animatedXForMaskedView}
+          cy={r}
+          r={r - 10}
+          strokeWidth={strokeWidth}
+        />
       </Group>
+      <Circle cx={animatedX} cy={r} r={r - 5} color="red" />
+      <Circle
+        color={'white'}
+        opacity={animatedZIndex}
+        cx={animatedXForMaskedView}
+        cy={r}
+        r={r - 15}
+        strokeWidth={strokeWidth}
+      />
     </Canvas>
   );
 };
--- src/components/FlickerLoading/hooks/useFlikerLoading.ts
+++ src/components/FlickerLoading/hooks/useFlikerLoading.ts
@@ -1,21 +1,27 @@
-import { mix, useSharedValueEffect, useValue } from '@shopify/react-native-skia';
-import { useEffect } from 'react';
-import {useSharedValue, withRepeat, withTiming} from "react-native-reanimated";
+import { mix, useClockValue, useSharedValueEffect, useValue } from '@shopify/react-native-skia';
+import { useEffect, useRef } from 'react';
+import { useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
 const useFlikerLoading = (r) => {
   const animatedX = useValue(r);
+  const test= useRef(1)
   const animatedXForMaskedView = useValue(3*r);
   const animatedZIndex = useValue(-1);
   const progress = useSharedValue(0);
- 
+  const skiaProgress = useValue(0)
+  const clock = useClockValue();
   useEffect(() => {
     progress.value = withRepeat(withTiming(1, { duration: 3000 }), -1, true);
   }, [progress]);
  
   useSharedValueEffect(() => {
-    animatedX.current = mix(progress.value, r , 3 * r);
-    animatedXForMaskedView.current = mix(progress.value, 3 * r, r);
-    animatedZIndex.current=mix(progress.value, -1, 1)
+    animatedX.current = mix(progress.value, r, 3 * r + 5);
+    animatedXForMaskedView.current = mix(progress.value, 3 * r + 5, r);
+    if (Number(progress.value) === 1.0 || (Number(progress.value) === 0.0)) {
+      console.log(progress.value,progress.value , 1.0,' progress.value?.toFixed(1)')
+      test.current = test.current === 0 ? 1 : 0;
+      animatedZIndex.current =  mix(test.current, 1, -1)
+    }   
   }, progress); // you can pass other shared values as extra parameters
  
   return {
